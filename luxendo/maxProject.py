540dccb1d6820a1cb5ff3c2ee26545a4b4230c2e
Noah P Mitchell <noah.prentice.mitchell@gmail.com>
2024-06-04 09:00:35 -0500

    update to maxProject naming files and ijm script to make avis

--- luxendo/maxProject.py
+++ luxendo/maxProject.py
@@ -6,16 +6,14 @@ from PIL import Image
 
 
 # Function to save MIP images
-
-def save_mip_images(mip, axis, filename, output_dir):
-    output_path = os.path.join(output_dir, f"{filename}_mip_axis_{axis}.tiff")
-    print('-->' + output_path)
-    Image.fromarray(mip).save(output_path)
+# def save_mip_images(mip, axis, filename, output_dir):
+#     output_path = os.path.join(output_dir, f"{filename}_mip_axis_{axis}.tiff")
+#     print('--> Saving to ' + output_path)
+#     Image.fromarray(mip).save(output_path)
 
 
 # Function to process a single directory
-
-def process_directory(input_dir, output_dir, plane=-1):
+def process_directory(input_dir, output_dir, dir_name, plane=-1):
     # Go through all directories in the input_dir directory, make MIPS of all subdirectories
     #
     # Parameters
@@ -27,46 +25,66 @@ def process_directory(input_dir, output_dir, plane=-1):
     #   if <0, use middle of the zstack as the plane of interest
     #
     os.makedirs(output_dir, exist_ok=True)
+    max_proj_dir = os.path.join(output_dir, 'mips')
+    os.makedirs(max_proj_dir, exist_ok=True)
+    midz_output_dir = os.path.join(output_dir, 'midZ')
+    os.makedirs(midz_output_dir, exist_ok=True)
+
     for filename in os.listdir(input_dir):
         if filename.endswith('.h5'):
-            filepath = os.path.join(input_dir, filename)
-            print('-> ' + filepath)
-            with h5py.File(filepath, 'r') as f:
-                data = f['/Data'][()]
-            mip_x = np.max(data, axis=0)
-            # mip_y = np.max(data, axis=1)
-            # mip_z = np.max(data, axis=2)
             base_filename = os.path.splitext(filename)[0]
-            save_mip_images(mip_x, 'x', base_filename, output_dir)
-            # save_mip_images(mip_y, 'y', base_filename, output_dir)
-            # save_mip_images(mip_z, 'z', base_filename, output_dir)
 
-            # Subset of z-stack as mip (mid-saggital, for ex)
-            if plane < 0:
-                # take middle z plane
-                plane = np.round(np.size(data, 0) * 0.5).astype(np.int64)
+            os.makedirs(os.path.join(max_proj_dir, dir_name+"_mips"), exist_ok=True)
+            os.makedirs(os.path.join(midz_output_dir, dir_name+"_midZ"), exist_ok=True)
+            max_proj_path = os.path.join(max_proj_dir, dir_name+"_mips", f"{base_filename}_mip_axis_x.tiff")
+            midz_path = os.path.join(midz_output_dir, dir_name+"_midZ", f"{base_filename}_midZ_axis_x.tiff")
 
-            mip_x = data[plane]
-            os.makedirs(output_dir + '_midZ', exist_ok=True)
-            save_mip_images(mip_x, 'x', base_filename, output_dir + '_midZ')
+            if not os.path.exists(max_proj_path) or not os.path.exists(midz_path):
+                filepath = os.path.join(input_dir, filename)
+                print('-> ' + filepath)
+                with h5py.File(filepath, 'r') as f:
+                    data = f['/Data'][()]
+                mip_x = np.max(data, axis=0)
+                # mip_y = np.max(data, axis=1)
+                # mip_z = np.max(data, axis=2)
+                Image.fromarray(mip_x).save(max_proj_path)
+                # save_mip_images(mip_y, 'y', base_filename, output_dir)
+                # save_mip_images(mip_z, 'z', base_filename, output_dir)
+
+                # Subset of z-stack as mip (mid-saggital, for ex)
+                if plane < 0:
+                    # take middle z plane
+                    plane = np.round(np.size(data, 0) * 0.5).astype(np.int64)
+
+                midz = data[plane]
+                Image.fromarray(midz).save(midz_path)
+
+            else:
+                print(f"--> Output file already exists, skipping: {max_proj_path}")
 
 
 if __name__ == "__main__":
 
     # parent_dir = 'D:\\rlondo\\HandGFPbynGAL4klar_H2AGFPUASMyo1CRFP\\2024-05-15_230441'  # Replace with your parent directory path
-    parent_dir = 'D:\\rlondo\\HandGFPbynGAL4klar_UASmChCAAXH2AviRFP\\2024-05-17_184833'  # Replace with your parent directory path
+    # parent_dir = 'D:\\rlondo\\HandGFPbynGAL4klar_UASmChCAAXH2AviRFP\\2024-05-17_184833'  # Replace with your parent directory path
+    # parent_dir = 'D:\\rlondo\\HandGFPbynGAL4klar_UASmChCAAXH2AviRFP\\2024-05-17_184833'  # Replace with your parent directory path
+
+    # parent_dir = 'E:\\haibei\\48YGAL4klar_UASmChCAAXHiRFP\\2024-05-23_183541'
+    # parent_dir = 'E:\\rio\\HandGFPbynGAL4klar_UASmChCAAXHiFP\\2024-05-27_125722_22C_butFocusDrifts'
+    # parent_dir = 'E:\\rio\\HandGFPbynGAL4klar_UASMyo1CRFPHiFP\\2024-05-28_150855'
+    # parent_dir = 'E:\\rio\\HandGFPbynGAL4klar_UASMyo1CRFPHiFP\\2024-05-28_150855_excellent'
+    # parent_dir = 'E:\\rio\\HandGFPbynGAL4klar_UASmChCAAXHiFP\\2024-05-31_142410'
+    parent_dir = 'E:\\haibei\\48YGAL4klar_UASLamGFPUASmChCAAX\\2024-06-03_142916'
 
     data_dir = os.path.join(parent_dir, 'raw')
-    outdir = os.path.join(parent_dir, 'mips')
     for dir_name in os.listdir(data_dir):
         input_dir = os.path.join(data_dir, dir_name)
         print(input_dir)
         if os.path.isdir(input_dir):
-            output_dir = os.path.join(outdir, dir_name)
-            print('processing ' + input_dir + " -> " + output_dir)
-            process_directory(input_dir, output_dir)
-            print('done with' + input_dir)
+            output_dir = parent_dir  # os.path.join(outdir, 'mips', dir_name)
+            print('processing ' + input_dir + " -> " + output_dir + "\\mips or midZ")
+            process_directory(input_dir, output_dir, dir_name)
+            print('done with ' + input_dir)
 
     print('done')
     print('Now drag all mips subdirectories into Fiji. Run maxProjectMovies_simple.ijm after changing path in line 5')
-
--- luxendo/maxProjectMovies_simple2.ijm
+++ luxendo/maxProjectMovies_simple2.ijm
@@ -1,8 +1,8 @@
 // Set the scale in microns per pixel
 scale = 0.19500001;
-interval = 2 ;
+interval = 3 ;
 nStacks = 3 ;
-fps = 7;
+fps = 5;
 
 // Specify the output directory
 outputDir = "D:/rlondo/HandGFPbynGAL4klar_UASmChCAAXH2AviRFP/2024-05-17_184833/mips/";  // Change this to your desired output directory
@@ -11,6 +11,47 @@ outputDir = "E:/rio/HandGFPbynGAL4klar_UASmChCAAXHiFP/2024-05-27_165010_22C_exce
 outputDir = "E:/rio/HandGFPbynGAL4klar_UASmChCAAXHiFP/2024-05-27_125722_22C_butFocusDrifts/" ;
 outputDir = "E:/rio/HandGFPbynGAL4klar_UASMyo1CRFPHiFP/2024-05-29_141924/";
 outputDir = "E:/rio/HandGFPbynGAL4klar_UASMyo1CRFPHiFP/2024-05-30_174009/";
+outputDir = "E:/haibei/48YGAL4klar_UASLamGFPUASmChCAAX/2024-06-03_142916/";
+
+
+//// 2 color [green magenta]
+//grnCh = 0 ;
+//magCh = 1 ;
+//
+//for (stck = 0; stck < nStacks; stck++) {
+//
+//	run("Merge Channels...", "c2=stack_"+stck+"_channel_"+grnCh+"_obj_left_mips c6=stack_"+stck+"_channel_"+magCh+"_obj_left_mips create");
+//	run("Enhance Contrast", "saturated=0.35");
+//	run("Label...", "format=00:00 starting=0 interval="+interval+" x=50 y=50 font=100 text=[] range=1-120 use");
+//	run("Set Scale...", "distance=1 known=" + scale + " unit=um");
+//	run("Scale Bar...", "width=50 height=50 thickness=20 font=100 bold overlay");
+//	saveAs("Tiff", outputDir + "Stack"+stck+"_left_composite_mips.tif");
+//
+//
+//	run("Merge Channels...", "c2=stack_"+stck+"_channel_"+grnCh+"_obj_right_mips c6=stack_"+stck+"_channel_"+magCh+"_obj_right_mips create");
+//	run("Enhance Contrast", "saturated=0.35");
+//	run("Label...", "format=00:00 starting=0 interval="+interval+" x=50 y=50 font=100 text=[] range=1-120 use");
+//	run("Set Scale...", "distance=1 known=" + scale + " unit=um");
+//	run("Scale Bar...", "width=50 height=50 thickness=20 font=100 bold overlay");
+//	saveAs("Tiff", outputDir + "Stack"+stck+"_right_composite_mips.tif");
+//	
+//
+//	run("Merge Channels...", "c2=stack_"+stck+"_channel_"+grnCh+"_obj_left_midZ c6=stack_"+stck+"_channel_"+magCh+"_obj_left_midZ create");	
+//	run("Enhance Contrast", "saturated=0.35");
+//	run("Label...", "format=00:00 starting=0 interval="+interval+" x=50 y=50 font=100 text=[] range=1-120 use");
+//	run("Set Scale...", "distance=1 known=" + scale + " unit=um");
+//	run("Scale Bar...", "width=50 height=50 thickness=20 font=100 bold overlay");
+//	saveAs("Tiff", outputDir + "Stack"+stck+"_left_composite_midZ.tif");
+//
+//
+//	run("Merge Channels...", "c2=stack_"+stck+"_channel_"+grnCh+"_obj_right_midZ c6=stack_"+stck+"_channel_"+magCh+"_obj_right_midZ create");
+//	run("Enhance Contrast", "saturated=0.35");
+//	run("Label...", "format=00:00 starting=0 interval="+interval+" x=50 y=50 font=100 text=[] range=1-120 use");
+//	run("Set Scale...", "distance=1 known=" + scale + " unit=um");
+//	run("Scale Bar...", "width=50 height=50 thickness=20 font=100 bold overlay");
+//	saveAs("Tiff", outputDir + "Stack"+stck+"_right_composite_midZ.tif");
+//}
+
 
 //// 3 color [cyan red white]
 //cyanCh = 1 ;
@@ -83,6 +124,7 @@ for (stck = 0; stck < 3; stck++) {
 
     // Get the name of the current image sequence
     baseName="Stack"+stck+"_left_composite_midZ.tif";
+	selectImage(baseName);
     // Construct the filenames for the AVI and Tiff files
     aviPath = outputDir + "Stack"+stck+"_left_composite_midZ.avi";
 
