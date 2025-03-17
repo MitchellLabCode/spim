f055a2685659b4553572e533fe58132ba7036533
Noah P Mitchell <noah.prentice.mitchell@gmail.com>
2025-03-17 12:41:56 -0500

    updates to DeleteTPsZFishHeart.py

--- zebrafishImaging/DeleteTPsZFishHeart.py
+++ zebrafishImaging/DeleteTPsZFishHeart.py
@@ -14,22 +14,30 @@ def del_timepoints(in_dir, out_dir, timepoint):
         if file_name_left in files:
             # Move the file to the output directory
             src_file = os.path.join(root, file_name_left)
-            shutil.move(src_file, os.path.join(out_dir, file_name_left))
+            dest = str(root[root.find("stack_"):root.find("obj")]) + file_name_left
+            shutil.move(src_file, os.path.join(out_dir, dest))
             print(f"Moved: {src_file} to {out_dir}")
 
         if file_name_right in files:
             # Move the file to the output directory
             src_file = os.path.join(root, file_name_right)
-            shutil.move(src_file, os.path.join(out_dir, file_name_right))
+            # get the subdir name
+            dest = str(root[root.find("stack_"):root.find("obj")]) + file_name_right
+            shutil.move(src_file, os.path.join(out_dir, dest))
             print(f"Moved: {src_file} to {out_dir}")
 
     print("Process completed.")
 
 
 if __name__ == "__main__":
-    in_dir = 'E:/sid/20250214_333fps_h2avGFPmemCh/2025-02-14_175827/raw'  # input directory path
-    out_dir = 'E:/sid/20250214_333fps_h2avGFPmemCh/To_be_deleted'  # output directory path
-    timepoint = 4  # The timepoint to be deleted
-    del_timepoints(in_dir, out_dir, timepoint)
+    in_dir = 'E:/sid/20250227_250fps_myl7eGFP/2025-02-27_181531/raw'  # input directory path
+    out_dir = 'E:/sid/20250227_250fps_myl7eGFP/To_be_deleted'  # output directory path
+
+    if not os.path.isdir(out_dir):
+        os.mkdir(out_dir)
+
+    timepoints = [0]  # The timepoints to be deleted
+    for tp in timepoints:
+        del_timepoints(in_dir, out_dir, tp)
 
 
