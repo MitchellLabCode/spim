39ae013adcff296556d7d1aaf893f206da398be8
Noah P Mitchell <noah.prentice.mitchell@gmail.com>
2025-10-12 21:54:11 -0400

    updated string replacements with current conventions

--- batch_rename_timestamps.py
+++ batch_rename_timestamps.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 
 # --- Config ---
-datdir = Path('/mnt/crunch/path/')
+datdir = Path('F:\\PROJECTS\\LightMicroscopyBootcamp2025\\bynGAL4klar_UASmChCAAXUASGFPnls\\20251011181532_unpacked\\')
 
 channels = [0, 1]
-angles = list(range(0, 361, 60))   # MATLAB 0:60:360 (inclusive)
-times = range(0, 200)              # MATLAB 0:199 (inclusive)
-addt = 5 
+angles = list(range(21, 361, 60))   # MATLAB 0:60:360 (inclusive)
+times = range(2, 3)              # MATLAB 0:199 (inclusive)
+addt = 0
 
 for ang in angles:
     for ch in channels:
@@ -15,14 +15,14 @@ for ang in angles:
             tout = tt + addt   # floor(tt * 0.5) + 2
 
             # expected input filename
-            filename = f"Time_{tt:06d}_Angle_{ang}_c{ch}_ls_1.ome.tif"
+            filename = f"stack_*_channel_{ch}_tp_{tt:05d}_angle_{ang}.ome.tif"
             matches = list(datdir.glob(filename))
 
             if len(matches) > 1:
                 raise RuntimeError(f"More than one match for filename: {filename}")
             elif len(matches) == 1:
                 src = matches[0]
-                dst = src.with_name(f"oTime_{tout:06d}_Angle_{ang}_c{ch}_ls_1.ome.tif")
+                dst = src.with_name(f"tp_{tout:06d}_c{ch}_angle_{ang}.ome.tif")
 
                 print(f"Seeking: {filename}")
                 print(f"renaming {src.name} > {dst.name}")
